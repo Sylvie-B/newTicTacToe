d14e3cc30c9cbab2d5d3ff0b95ec2aa5c1bed7d2
Sylvie-B <unsolo@hotmail.fr>
2020-12-10 15:25:40 +0100

    live ap-midi

--- appTTT.js
+++ appTTT.js
@@ -1,8 +1,7 @@
 const CLICK_LEFT = 0;
 const CLICK_RIGHT = 2;
-
+let playerX = false, playerO = false;
 let cases = document.getElementsByClassName("case");
-console.log(cases);
 
 for(let square of cases){
     square.addEventListener('mouseup', function (event){
@@ -10,58 +9,61 @@ for(let square of cases){
             case CLICK_LEFT:
                 // si la case est vide alors...
                 insertPlayerTexte(this, 'X');
-                horizon();
-                console.log(horizon());
-
                 break;
             case CLICK_RIGHT:
                 insertPlayerTexte(this, 'O');
                 break;
         }
         checkCases();
-    })
+    });
 }
 
 function checkCases() {
-    horizon();
-    verticale();
-    diago();
+    playerX = horizon('X'); //  || verticale('X') || diago('X')
+    playerO = horizon('O');
+
+    if (!playerX && !playerO) {
+        playerX = verticale('X');
+        playerO = verticale('O');
+
+        if (!playerX && !playerO) {
+            playerX = diago('X');
+            playerO = diago('O');
+        }
+
+    }
+        if(playerX) {
+            document.getElementById('won').innerHTML = "le joueur 1 a gagné (X)";
+        }
+        else if (playerO){
+            document.getElementById('won').innerHTML = "le joueur 2 a gagné (Y)";
+        }
 }
 
-function horizon (){
-    // une verif horizontale
-    // test 0,1,2
-    // if(cases[0].innerHTML === 'X' && cases[1].innerHTML === 'X' && cases[2].innerHTML === 'X'){
-    //     return 'player 1 won';
-    // }
-    // else if (cases[0].innerHTML === 'O' && cases[1].innerHTML === 'O' && cases[2].innerHTML === 'O'){
-    //     return 'player 2 won';
-    // }
-    // return false;
-    // test 3,4,5
-    // test 6,7,8
-    for(let index = 0; index <= 8; index+=3){
-        if(cases[0].innerHTML === 'x' && cases[index+1].innerHTML && cases[index+2]){
-            return 'player 1 won';
+function horizon (player){
+
+    for(let idx = 0; idx <= 8; idx+=3){
+        if(cases[idx].innerHTML === player && cases[idx+1].innerHTML === player && cases[idx+2] === player){
+            return true;
         }
     }
+    return false;
 }
 
-function verticale (){
+function verticale (player){
     // une verif verticale
-    // test 0, 3, 6
-    // test 1, 4, 7
-    // test 2, 5, 8
+    for(let idx = 0; idx <= 2; idx+=1){
+        if(cases[idx].innerHTML === player && cases[idx+3].innerHTML === player && cases[idx+6] === player){
+            return true;
+        }
+    }
+    return false;
 }
 
-function diago (){
-    // une verfif diagonale
-    // test 0, 4, 8
-    // test 6, 4, 2
+function diago (player){
+    return cases[4].innerHTML === player && (cases[0].innerHTML === player && cases[8].innerHTML === player) ||
+                                            (cases[2].innerHTML === player && cases[6].innerHTML === player)
 }
-
-
-
 
 function insertPlayerTexte (element, playerChar){
     if (element.innerHTML.length === 0){
@@ -72,5 +74,14 @@ function insertPlayerTexte (element, playerChar){
 
 document.addEventListener('contextmenu', function (event){
     event.preventDefault();
-})
+});
+
+document.getElementById("reset").addEventListener('click', function (){
+    for(let square of cases) {
+        square.innerHTML = "";
+    }
+    playerO = false;
+    playerX = false;
+    document.getElementById('won').innerHTML = "";
+});
 
